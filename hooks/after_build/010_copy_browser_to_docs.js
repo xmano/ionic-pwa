5f10e68e87e47a27efc8492d483519b35483a888
Mano <mano@xkanda.com>
2017-05-19 17:19:21 +0530

    Update.

--- hooks/after_build/010_copy_browser_to_docs.js
+++ hooks/after_build/010_copy_browser_to_docs.js
@@ -9,10 +9,10 @@ var shell = require('child_process').execSync;
 var fs = require('fs');
 var path = require('path');
 
-const src= 'platforms/browser/www';
+const src= './platforms/browser/www';
 const dist= 'docs';
 
-if(process.env.CORDOVA_PLATFORMS == "browser"){
+if (process.env.CORDOVA_PLATFORMS == "browser"){
   shell('mkdir -p ${dist}');
   shell('cp -r ${src}/* ${dist}');
 }
